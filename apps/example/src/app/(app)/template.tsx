9dcd404f50c2f56bafa9d401e711ea99bf445381
Claudio Fuentes <claudio@trycomp.ai>
2026-01-08 21:50:51 -0500

    style: change fade easing to ease-in-out

--- apps/example/src/app/(app)/template.tsx
+++ apps/example/src/app/(app)/template.tsx
@@ -21,7 +21,7 @@ export default function Template({ children }: TemplateProps) {
     <div
       style={{
         opacity,
-        transition: 'opacity 1000ms ease-out',
+        transition: 'opacity 1000ms ease-in-out',
       }}
     >
       {children}
